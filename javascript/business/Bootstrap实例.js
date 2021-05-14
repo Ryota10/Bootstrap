f8dd44f9d20fc094b9a1cd1d566df2e111508ae6
Gatsby <liugatsby17@gmail.com>
2021-05-14 14:18:00 +0800

    补充完成Bootstrap实例

--- javascript/business/Bootstrap实例.js
+++ javascript/business/Bootstrap实例.js
@@ -1,7 +1,33 @@
 $(function () {
+    setScroll()
+    setCarousel();
+    setDropDownCon();
+})
+
+//刷新页面时强制滚动条回到顶部
+function setScroll() {
+    $(this).on('beforeunload', function () {
+        $(this).scrollTop(0);
+    });
+}
+
+//设置轮播事件
+function setCarousel() {
     $(".carousel").carousel({
         interval: 3000,
         pause: null,
         wrap: true
     })
-})
+}
+
+//设置 nav——标签页 下拉框联动
+function setDropDownCon() {
+    $("#about-nav>li>a").click(function (e) {
+        e.preventDefault();
+        var $href = $(this).attr("href");
+        $("#self-tablist>li>a[href=" + $href + "]").tab('show');
+
+        //滚动条定位到标签页
+        $(document).scrollTop($("#self-bookmarkPage").offset().top);
+    });
+}
